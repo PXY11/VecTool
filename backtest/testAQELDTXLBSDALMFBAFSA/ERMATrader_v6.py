1e6901b6c94f4b3bd15fd1fe0cc44195cc0ac4ba
PXY11 <1056827244@qq.com>
2021-11-04 15:51:00 +0800

    新增20个币的ERSign测试，每次选ER最大的4个

---
+++ backtest/testAQELDTXLBSDALMFBAFSA/ERMATrader_v6.py
@@ -0,0 +1,60 @@
+from os import close
+from pickle import FALSE
+import sys
+sys.path.append('../../')
+from vector import portfolio
+import pandas as pd
+from datetime import datetime, timedelta
+
+class Trader(portfolio.Portfolio):
+    def init(self, symbols: list):
+        super().init(symbols)
+        self.order_ids = {}
+        for symbol in symbols:
+            self.order_ids[symbol] = ""
+        self.symbols = symbols
+        self.init_cash = 100000
+    
+    def algorithm(self,s:dict):
+        er_dict = {}
+        for symbol in self.symbols:
+            er_dict[symbol] = s[symbol]['er']
+        er_list = sorted(er_dict.items(), key=lambda item:item[1]) #[(symbol,er)]
+        er_max_symbols = [er_list[-1][0],er_list[-2][0],er_list[-3][0],er_list[-4][0]] #ER最大的4个币种
+        # er_threshold = sum([er_list[i][1] for i in range(len(er_list))])/len(er_list)
+        # er_threshold = er_list[3][1] #er_threshold为全品种的er中位数
+
+        for symbol in er_max_symbols:
+            '''
+            对于er最大的前1（2）个币种，若满足
+            1、DEMA大于VWAP
+            则开仓做多
+            '''
+            signal_open = s[symbol]['DEMAoverVWAP']
+            price = s[symbol]['close']
+            time = s['datetime']
+            if not self.order_ids[symbol]:
+                if signal_open == True and er_dict[symbol]>0: #信号进
+                    print(time,f'【{symbol}】DEMA is 【OVER】 VWAP and its ER is big, Long it.')
+                    order_id = self.entry_order(symbol,round(1*self.init_cash/price, 2))
+                    self.order_ids[symbol] = order_id
+        
+        for symbol in self.symbols:
+            '''
+            对于所有币种，检查是否有持仓，进一步判断
+            1、信号变为False，平仓
+            '''           
+            if self.order_ids[symbol]:
+                signal_close = s[symbol]['DEMAoverVWAP']
+                if signal_close == False: #信号出
+                    print(f'{symbol} is held,【siganl change to False】, close order ', self.order_ids[symbol])
+                    op = self.get_order(self.order_ids[symbol])
+                    self.exit_order(op)
+                    
+    def on_order(self, order: portfolio.Order):
+        if order.status == portfolio.OrderStatus.Finished:
+            self.order_ids[order.symbol] = ""
+
+
+
+
---
+++ indicator/AutoGetIndicatorAQELDTXLBSDALMFBAFSA.py
@@ -0,0 +1,51 @@
+
+from indicatorTool import *
+
+'''
+用于测试indicatorTool
+'''
+
+#updater_csi = Updater(DataToolparamVersion = '_v3',DataToolremark = '_SFUDFX',instanceId=1)
+#res_csi = updater_csi.get_new(factor='csi',
+#                                  SignalCalculatorparamVersion='_v3',
+#                                  SignalCalculatoremark='_csi',
+#                                  save=False,
+#                                  upload = False,
+#                                  tableNameExt='SFUDFX')
+#
+#updater_roc = Updater(DataToolparamVersion = '_v3',DataToolremark = '_SFUDFX',instanceId=2)
+#res_roc = updater_roc.get_new(factor='roc',
+#                                 SignalCalculatorparamVersion='_v3',
+#                                 SignalCalculatoremark='_roc',
+#                                 save=False,
+#                                 upload=False,
+#                                 tableNameExt='SFUDFX')
+#
+#updater_cci = Updater(DataToolparamVersion = '_v3',DataToolremark = '_SFUDFX',instanceId=3)
+#res_cci = updater_cci.get_new(factor='cci',
+#                                 SignalCalculatorparamVersion='_v3',
+#                                 SignalCalculatoremark='_cci',
+#                                 save=False,
+#                                 upload=False,
+#                                 tableNameExt='SFUDFX')
+##%%
+#updater_ar = Updater(DataToolparamVersion = '_v3',DataToolremark = '_SFUDFX',instanceId=4)
+#res_ar = updater_ar.get_new(factor='absorptionRatio',
+#                                SignalCalculatorparamVersion='_v3',
+#                                SignalCalculatoremark='_ar',
+#                                save=False,
+#                                upload=False,
+#                                tableNameExt='SFUDFX')
+
+
+updater_er = Updater(DataToolparamVersion = '_v12',DataToolremark = '_AQELDTXLBSDALMFBAFSA',instanceId=5)
+res_er = updater_er.get_new(factor='efficiencyRatioSign',
+                                SignalCalculatorparamVersion='_v12',
+                                SignalCalculatoremark='_ersign',
+                                save=True,
+                                upload=False,
+                                tableNameExt='AQELDTXLBSDALMFBAFSA')
+                                  
+                                    
+                                    
+                                    
